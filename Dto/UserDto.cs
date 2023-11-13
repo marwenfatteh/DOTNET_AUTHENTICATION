641bc806a5a27c5175db553c791b6e55135ef12f
fateh <fateh@MARWEN>
2023-11-13 17:30:40 +0100

    Ajoutez des fichiers projet.

---
+++ Dto/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DOTNET_AUTHENTICATION.Dto
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Pasword { get; set; }
+    }
+}
